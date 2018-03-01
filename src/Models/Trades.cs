ac2624e541e77cafbaa4909c1d305f8cfd2b6e5a
EmptyShadow <dimon6407@gmail.com>
2018-03-01 18:21:34 +0300

    Add model trades

--- src/Models/Trades.cs
+++ src/Models/Trades.cs
@@ -4,7 +4,40 @@ using System.Text;
 
 namespace Trades.src.Models
 {
-    class Trades
+    /// <summary>
+    /// Модель сделки
+    /// </summary>
+    [Serializable]
+    public class Trades
     {
+        /// <summary>
+        /// Индитификатор
+        /// </summary>
+        public int id;
+        /// <summary>
+        /// Стоимость
+        /// </summary>
+        public int price;
+        /// <summary>
+        /// Количество
+        /// </summary>
+        public int quantity;
+        /// <summary>
+        /// Позиция
+        /// </summary>
+        public string side;
+        /// <summary>
+        /// Время сделки
+        /// </summary>
+        public DateTime dateTime;
+
+        public Trades(int id, int price, int quantity, string side, DateTime dateTime)
+        {
+            this.id = id;
+            this.price = price;
+            this.quantity = quantity;
+            this.side = side;
+            this.dateTime = dateTime;
+        }
     }
 }
